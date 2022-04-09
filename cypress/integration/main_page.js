cdb19677e6f438e343f7dbc0f4fb74c5895f6c75
Ozansacmaz <ozansacmaz@gmail.com>
2022-04-09 21:31:14 +0300

    initial commit setup

---
+++ cypress/integration/main_page.js
@@ -0,0 +1,14 @@
+/// <reference types = "cypress"/>
+
+it('Rapsodo QA Assignment', ()=>{
+
+
+    cy.visit('http://demowebshop.tricentis.com/')
+
+    //cy.get('.gLFyf').type("Ozan SACMAZ {enter}")
+
+    //cy.get('.MenuHeader > :nth-child(1) > .Link').click()
+    
+
+
+})
